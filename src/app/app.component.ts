96db4c3bae23bde61546b5f915de0c92d5d8ef5f
luizgcancian <luizgnr@hotmail.com>
2023-05-22 15:44:59 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name = '';
+  date = '';
+  amount = '';
+  miles: number = 0;
+  onNameChange(value: string) {
+    this.name = value;
+  }
+
+  onDateInput(value: string) {
+    this.date = value;
+  }
+
+  onAmountInput(value: string) {
+    this.amount = value;
+  }
+
+  onMilesChange(value: string) {
+    this.miles = Number(value);
+  }
 }
---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert',
+})
+export class ConvertPipe implements PipeTransform {
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return Number(value) * 1.60934;
+  }
+}
